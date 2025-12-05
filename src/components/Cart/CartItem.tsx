7af839aca6fba9e3e2b8a2e65f6d856e36ed228e
jackal7819 <jackal3166@gmail.com>
2025-12-05 21:09:05 +0200

    feat: add Cart component

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -1,14 +1,26 @@
-export default function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+import { formatCurrency } from '../../utils/helpers';
 
-  return (
-    <li>
-      <p>
-        {quantity}&times; {name}
-      </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
-      </div>
-    </li>
-  );
+interface CartItemProps {
+	item: {
+		pizzaId: number;
+		name: string;
+		quantity: number;
+		unitPrice: number;
+		totalPrice: number;
+	};
+}
+
+export default function CartItem({ item }: CartItemProps) {
+	const { pizzaId, name, quantity, totalPrice } = item;
+
+	return (
+		<li>
+			<p>
+				{quantity}&times; {name} {pizzaId}
+			</p>
+			<div>
+				<p>{formatCurrency(totalPrice)}</p>
+			</div>
+		</li>
+	);
 }
